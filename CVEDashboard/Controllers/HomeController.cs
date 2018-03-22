67b2548d24796c30447f80bc72ff2c8e49f97655
Micheal Corr <m.corr@kainos.com>
2018-03-22 12:52:31 +0000

    DB functions

--- CVEDashboard/Controllers/HomeController.cs
+++ CVEDashboard/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CVEDashboard.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,20 @@ namespace CVEDashboard.Controllers
 {
     public class HomeController : Controller
     {
+
+
         public ActionResult Index()
         {
-            return View();
+            DBMethods dbMethods = new DBMethods();
+            SqlDataReader reader = dbMethods.getReader("SELECT name FROM tbl_names WHERE id = 1");
+            String name = "";
+            while (reader.Read())
+            {
+                 name = reader.GetString(0);
+            }
+
+            ViewBag.returnName = name;
+            return View((object)name);
         }
 
     }
---
+++ CVEDashboard/Models/DBMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace CVEDashboard.Models
+{
+    public class DBMethods
+    {
+
+        private SqlConnection connection;
+
+        public SqlConnection getConnection()
+        {
+            string connectionString = "";
+
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            Trace.WriteLine("Connection Established");
+
+            return connection;
+        }
+
+        public SqlDataReader getReader(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Connection = this.getConnection();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Trace.WriteLine("Reading.....");
+            return reader;
+        }
+
+    }
+}
